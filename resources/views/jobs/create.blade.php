e2b4cc66b8dbacd7b0710931de9528740dce65a9
crazymarion820 <crazymarion820@gmail.com>
2016-03-14 23:54:16 +0800

    add views

---
+++ resources/views/jobs/create.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.main')
+@section('content')
+    <h1>{{ $user->name }} Create Jobs</h1>
+    <hr>
+@stop
---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel Crazy Site</title>
+    <!-- Twitter Bootstrap -->
+    <link href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    @yield('content')
+    @yield('footer')
+</div>
+</body>
+</html>
