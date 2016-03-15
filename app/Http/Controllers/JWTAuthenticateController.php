d6b06acff6cc625cdf39da90b29c463e6ff6542c
crazymarion820 <crazymarion820@gmail.com>
2016-03-15 15:55:25 +0800

    add get all jobs api by using JWT

--- app/Http/Controllers/JWTAuthenticateController.php
+++ app/Http/Controllers/JWTAuthenticateController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Job;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -10,9 +11,18 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 
 class JWTAuthenticateController extends Controller
 {
+    public function __construct()
+    {
+        // Apply the jwt.auth middleware to all methods in this controller
+        // except for the authenticate method. We don't want to prevent
+        // the user from retrieving their token if they don't already have it
+        $this->middleware('jwt.auth', ['except' => ['authenticate']]);
+    }
+
     public function index()
     {
-
+        $jobs = Job::all();
+        return $jobs;
     }
 
     public function authenticate(Request $request)
