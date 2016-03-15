edf421f893e88fbe82046cd20c5a7515e2930107
crazymarion820 <crazymarion820@gmail.com>
2016-03-15 15:45:25 +0800

    JWT get Auth Token OK!!!

--- app/Http/Controllers/JWTAuthenticateController.php
+++ app/Http/Controllers/JWTAuthenticateController.php
@@ -6,8 +6,30 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class JWTAuthenticateController extends Controller
 {
-    //
+    public function index()
+    {
+
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        try {
+            // verify the credentials and create a token for the user
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 401);
+            }
+        } catch (JWTException $e) {
+            // something went wrong
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+
+        // if no errors are encountered we can return a JWT
+        return response()->json(compact('token'));
+    }
 }
